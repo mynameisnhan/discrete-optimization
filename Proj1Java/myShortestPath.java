de25fb7fbf8b303172f95bfa7488b2ee33d2bf1c
Nhan Le <nle10147@yahoo.com>
2020-11-22 17:31:23 +0100

    MST done

--- Proj1Java/myShortestPath.java
+++ Proj1Java/myShortestPath.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
-//Implements shortest path algorithms. Can return distances to vertices from a given start vertex and the corresponding shortest path.
+// Implements shortest path algorithms. Can return distances to vertices from a given start vertex and the corresponding shortest path.
 public class myShortestPath {
-	//Variables
-	private Graph<Integer, DefaultWeightedEdge> graph;			//The graph in which we search for a shortest path
-	private Integer startVertex;								//The start vertex
-	private HashMap<Integer, Double> distances;					//Distances from each vertex to the start vertex
-	private HashMap<Integer, Integer> predecessors;				//Predecessors for each vertex on a shortest path to the start vertex
+	// Variables
+	private Graph<Integer, DefaultWeightedEdge> graph;			// The graph in which we search for a shortest path
+	private Integer startVertex;								// The start vertex
+	private HashMap<Integer, Double> distances;					// Distances from each vertex to the start vertex
+	private HashMap<Integer, Integer> predecessors;				// Predecessors for each vertex on a shortest path to the start vertex
 	
-	//Constructor
+	// Constructor
 	public myShortestPath(Graph<Integer, DefaultWeightedEdge> graph, Integer startVertex) {
 		this.graph = graph;
 		this.startVertex = startVertex;
@@ -21,100 +21,31 @@ public class myShortestPath {
 		predecessors = new HashMap<Integer, Integer>();
 	}
 	
-	//Computes distances and predecessors for all nodes
+	// Computes distances and predecessors for all nodes
 	public void computeDistPred() {
 		for (Integer vertex : graph.vertexSet()) {
 			distances.put(vertex, Double.MAX_VALUE);
 			predecessors.put(vertex, null);
         }
 		distances.put(startVertex, 0.);
-		
-		//START OF OUR CODE
-		public void bellmanFord(){
-			//Distance to all vertices is inf and pred is null
-			for (Integer vertex : graph.vertexSet()) {
-				distances.put(vertex, Double.MAX_VALUE);
-				predecessors.put(vertex, null);
-			}
-			//Distance to start vertex is zero
-			distances.put(startVertex, 0.);
-	
-			//V-1 times
-			for (int i = 1; i < graph.vertexSet().size()-1; i++) {
-				//relax each edge
-				for (var edge: graph.edgeSet()) {
-					//Get vertices of edge and respective weight
-					Integer u = graph.getEdgeSource(edge);
-					Integer v = graph.getEdgeTarget(edge);
-					double weight = graph.getEdgeWeight(edge);
-	
-					//Currently checks edge both ways
-					//TODO: directed graph handling
-					if(distances.get(u) + weight < distances.get(v)) {
-						distances.put(v, distances.get(u) + weight);
-						predecessors.put(v, u);
-					}else if(distances.get(v) + weight < distances.get(u)){
-						distances.put(u, distances.get(v) + weight);
-						predecessors.put(u, v);
-					}
-	
-				}
-			}
-		}
-	
-		/**
-		 * Dijkstra's algorithm
-		 */
-		public void dijkstra() {
-			Map<Integer,Boolean> colour = new HashMap<>();
-			//Initialize all distances as Inf, pred as null
-			for (Integer vertex : graph.vertexSet()) {
-				distances.put(vertex, Double.MAX_VALUE);
-				predecessors.put(vertex, null);
-				colour.put(vertex,Boolean.FALSE);
-			}
-			//Add start vertex with distance = 0
-			distances.put(startVertex, 0.);
-			colour.put(startVertex,Boolean.TRUE);
-			//Update all neighbours of start vertex
-			adjacent(startVertex)
-					.filter(x->colour.get(x)==Boolean.FALSE)
-					.forEach(x->updateVertexDistance(startVertex,x));
-	
-			//Only works on graphs where a path to every other node is possible (as of now)
-			//TODO: break if only unreachable nodes remain
-			for (int i = 0; i < graph.vertexSet().size()-1; i++) {
-				//Get vertex,distance pair with minimum distance
-				Map.Entry<Integer, Double> min = Collections.min(
-						distances.entrySet().stream()
-								.filter(x->colour.get(x.getKey())==Boolean.FALSE) //Only consider unadded vertices
-								.collect(Collectors.toList()),
-						Map.Entry.comparingByValue());
-				//Set the minimum vertex to added
-				colour.put(min.getKey(),Boolean.TRUE);
-				//Update all neighbours added vertex
-				adjacent(min.getKey())
-						.filter(x->colour.get(x)==Boolean.FALSE)
-						.forEach(x->updateVertexDistance(min.getKey(),x));
-			}
-		}
-	
-		private Stream<Integer> adjacent(Integer start){
-			return graph.vertexSet().stream().filter(x-> graph.containsEdge(start,x));
-		}
-	
-		private void updateVertexDistance(Integer source, Integer target){
-			if(distances.get(source) + graph.getEdgeWeight(graph.getEdge(source,target)) < distances.get(target)){
-				distances.put(target, distances.get(source) + graph.getEdgeWeight(graph.getEdge(source,target)));
-				predecessors.put(target, source);
-			}
-		}
-		// END OF OUR CODE
 
+		/*
+		*
+		* START OF OUR CODE
+		*
+		*/
+
+		// Dijkstra's algorithm
+		
+		/*
+		*
+		* END OF OUR CODE
+		*
+		*/
 	}
 	
 	
-	//Constructs the shortest path from the start node to a given end node using the list of predecessors
+	// Constructs the shortest path from the start node to a given end node using the list of predecessors
 	public ArrayList<Integer> constructPathToNode(Integer endVertex) {
 		ArrayList<Integer> path = new ArrayList<Integer>();
 		path.add(endVertex);
@@ -127,7 +58,7 @@ public class myShortestPath {
 		return path;
 	}
 		
-	//Returns the distance to a given end node
+	// Returns the distance to a given end node
 	public double getDistanceToNode(Integer endVertex) {
 		return distances.get(endVertex);
 	}
--- Proj1Java/mySpanningTree.java
+++ Proj1Java/mySpanningTree.java
@@ -7,15 +7,13 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.AsSubgraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import java.util.Iterator; // OUR CODE
-import java.util.Set; // OUR CODE
-import java.util.Hashtable; // OUR CODE
 		
 public class mySpanningTree {
 	
 	// Receives a graph and computes a minimum spanning tree
 	public static AsSubgraph<Integer, DefaultWeightedEdge> computeMST(Graph<Integer, DefaultWeightedEdge> graph) {
 		AsSubgraph<Integer, DefaultWeightedEdge> tree = new AsSubgraph<Integer, DefaultWeightedEdge>(graph, graph.vertexSet(), new HashSet<DefaultWeightedEdge>());
-
+			
 		/*
 		*
 		* START OF OUR CODE
@@ -24,33 +22,34 @@ public class mySpanningTree {
 
 		// Kruskal's algorithm
 		
+		HashSet<Integer> vertexSet = new HashSet<Integer>(graph.vertexSet());
+	
 		// Sort edges by ascending order of weight values
 		Iterator<DefaultWeightedEdge> edgeSet = graph.edgeSet().stream().sorted(Comparator.comparingDouble(graph::getEdgeWeight)).iterator();
-    
-		Set<Integer> vertexSet = graph.vertexSet();	
-		
+ 
 		// Initialize connected components
-		Hashtable<Integer, Integer> connectedComponents = new Hashtable<>();
-		for (Integer vertex: vertexSet) {
-            connectedComponents.put(vertex, vertex);
+		HashMap<Integer, Integer> connectedComponents = new HashMap<>();		
+		for (int vertex: vertexSet) {
+			connectedComponents.put(vertex, vertex);
 		}
-
+		
 		// For each edge...
-		while(edgeSet.hasNext()) {
+		while (edgeSet.hasNext()) {
 			DefaultWeightedEdge edge = edgeSet.next();
-			
+			int edgeSource = connectedComponents.get(graph.getEdgeSource(edge));
+			int edgeTarget = connectedComponents.get(graph.getEdgeTarget(edge));
+
 			// ...if its addition would not introduce a cycle...
-			if(!connectedComponents.get(graph.getEdgeSource(edge))
-			.equals(connectedComponents.get(graph.getEdgeTarget(edge)))){
+			if (edgeSource != edgeTarget) {
 				// ...add it to the tree
-				tree.addEdge(graph.getEdgeSource(edge), graph.getEdgeTarget(edge), edge);
-				for (Integer vertex: vertexSet) {
-					if (connectedComponents.get(vertex).equals(connectedComponents.get(graph.getEdgeTarget(edge)))){
-						connectedComponents.put(vertex, connectedComponents.get(graph.getEdgeSource(edge)));
+				tree.addEdge(graph.getEdgeSource(edge),graph.getEdgeTarget(edge),edge);
+				for (int vertex: vertexSet) {
+					if (connectedComponents.get(vertex).equals(edgeTarget)){
+						connectedComponents.put(vertex, edgeSource);
 					}
-				}
+				}		
 			}
-		} 
+		}
 
 		/*
 		*
@@ -60,4 +59,5 @@ public class mySpanningTree {
 
 		return tree;
 	}
+
 }
